6eff2544b1d1d3a7070536ec637b9821ee305089
Daniel Kamenov <sorostmcoc@gmail.com>
2023-08-26 11:07:25 +0300

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -57,3 +57,40 @@ print(df2.describe())
 #dataframe.groupby('columnName').agg(np.mean) # find the average across all columns for every unique columnName
 #dataframe['column] = dataframe['column'].astype(float) # convert the datatype of the series to float
 #dataframe['column'] = pd.Categorical(dataframe['column'], [1,2,3,4], ordered=True) # convert the datatype of the series to categorical with ordering
+#dataframe.sort_values('column1') # sort values by column1 in ascending order
+#dataframe.sort_values('column2',ascending=False) # sort values by column2 in descending order
+#dataframe.sort_values(['column1','column2'],ascending=[True,False]) # sort values by column1 in ascending order then column2 in descending order
+#dataframe['column1'].unique() # get unique values in column1
+#dataframe['column1'].nunique() # get number of unique values in column1
+#dataframe['column1'].value_counts() # get number of times each value in column1 occurs
+#dataframe['column1'].value_counts(normalize=True) # get percentage of times each value in column1 occurs
+#dataframe['column1'].apply(function) # apply function to values in column1
+#dataframe['column1'].apply(lambda x: x*2) # apply function to values in column1
+#dataframe.dropna() # drop all rows that contain null values
+#dataframe.dropna(axis=1) # drop all columns that contain null values
+#dataframe.dropna(axis=1,thresh=n) # drop all rows have have less than n non null values
+#dataframe.fillna(x) # replace all null values with x
+#dataframe['column1'].fillna(dataframe['column1'].mean()) # replace all null values in column1 with the mean of column1
+#dataframe.duplicated() # returns true for every row that is a duplicate, false otherwise
+#dataframe.drop_duplicates() # drop all duplicate rows
+#dataframe.drop_duplicates(['column1','column2']) # drop all duplicate rows based on column1 and column2
+#dataframe.rename(columns={'oldName1':'newName1','oldName2':'newName2'}) # rename columns
+#dataframe.set_index('column1') # change the index
+#dataframe.rename(index={0:'firstEntry',1:'secondEntry'}) # rename index
+#dataframe.rename(index=str.lower) # change the index to lowercase
+#dataframe.rename(columns=str.lower) # change the column names to lowercase
+#pd.concat([dataframe1,dataframe2,dataframe3]) # concatenate dataframes together in order
+#pd.concat([dataframe1,dataframe2,dataframe3],axis=1) # concatenate dataframes together in order
+#dataframe1.append(dataframe2) # append dataframe2 to dataframe1
+#dataframe1.append(dataframe2,ignore_index=True) # append dataframe2 to dataframe1 and reset the index
+#dataframe.join(dataframe2,on='column1',how='inner') # SQL-style join the columns in dataframe1 and dataframe2
+#dataframe.merge(dataframe2,on='column1') # merge dataframe1 and dataframe2 together
+#dataframe.merge(dataframe2,left_on='column1',right_on='column2') # merge dataframe1 and dataframe2 together
+#dataframe.pivot_table(index='column1',values='column2',aggfunc=np.mean) # create pivot table that groups by column1 and calculates the mean of column2
+#dataframe.pivot_table(index='column1',values='column2',aggfunc=np.mean,columns='column3') # create pivot table that groups by column1 and calculates the mean of column2 and column3
+#dataframe.groupby('column1').agg(np.mean) # find the average across all columns for every unique column1
+#dataframe.apply(np.mean) # apply the function np.mean() across each column
+#dataframe.apply(np.max,axis=1) # apply the function np.max() across each row
+#dataframe.apply(lambda x: x.max() - x.min()) # apply the function x.max() - x.min() across each column
+#dataframe['column1'].apply(lambda x: 1 if x > 0 else 0) # apply the function x > 0 ? 1 : 0 across each value in column1
+
