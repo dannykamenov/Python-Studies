d7f12367d34e527318ea81ff9ab26c4c1475d0fd
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-04 15:23:22 +0300

    Update main.js

--- Small Projects/Background Remover/main.js
+++ Small Projects/Background Remover/main.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch('http://127.0.0.1:5000/process', {
             method: 'POST',
             body: formData,
-            mode: 'no-cors'
         })
         .then(response => response.blob())
         .then(blob => {
