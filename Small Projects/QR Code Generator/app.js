b837ce6f346073370a3b803edc046bc3a00fa3e2
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-10 16:13:00 +0300

    Update app.js

--- Small Projects/QR Code Generator/app.js
+++ Small Projects/QR Code Generator/app.js
@@ -4,18 +4,24 @@ document.getElementById('qrForm').addEventListener('submit', function(event) {
     const dataInput = document.getElementById('dataInput');
     const qrImage = document.getElementById('qrImage');
 
-    fetch('https://qr-code-generator-hvve.onrender.com/generate_qr', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ data: dataInput.value })
-    })
-    .then(response => response.json())
-    .then(data => {
-        qrImage.src = 'data:image/png;base64,' + data.qr_code;
-    })
-    .catch(error => {
-        console.error('Error generating QR code:', error);
-    });
+    // Check if input is link
+    if (dataInput.value.includes('http') || dataInput.value.includes('https')) {
+        fetch('https://qr-code-generator-hvve.onrender.com/generate_qr', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data: dataInput.value })
+        })
+        .then(response => response.json())
+        .then(data => {
+            qrImage.src = 'data:image/png;base64,' + data.qr_code;
+        })
+        .catch(error => {
+            console.error('Error generating QR code:', error);
+        });
+    }else {
+        alert('Please enter a valid link!');
+    }
+
 });
