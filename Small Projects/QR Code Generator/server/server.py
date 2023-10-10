10d82f7fb54f192bfc1a484fcae369acf169e6d3
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-10 15:39:24 +0300

    Update server.py

--- Small Projects/QR Code Generator/server/server.py
+++ Small Projects/QR Code Generator/server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template_string, request
 import qrcode
 from io import BytesIO
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
