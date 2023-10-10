58f0ad3ea1f7e4954e9df1b7c1db01c5ee4ec11e
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-10 16:23:09 +0300

    updates

--- Small Projects/QR Code Generator/app.js
+++ Small Projects/QR Code Generator/app.js
@@ -3,6 +3,9 @@ document.getElementById('qrForm').addEventListener('submit', function(event) {
 
     const dataInput = document.getElementById('dataInput');
     const qrImage = document.getElementById('qrImage');
+    if (qrImage) {
+        qrImage.remove();
+    }
 
     // Check if input is link
     if (dataInput.value.includes('http') || dataInput.value.includes('https')) {
@@ -15,7 +18,11 @@ document.getElementById('qrForm').addEventListener('submit', function(event) {
         })
         .then(response => response.json())
         .then(data => {
+            const body = document.querySelector('body');
+            const qrImage = document.createElement('img');
+            qrImage.setAttribute('id', 'qrImage');
             qrImage.src = 'data:image/png;base64,' + data.qr_code;
+            body.appendChild(qrImage);
         })
         .catch(error => {
             console.error('Error generating QR code:', error);
@@ -23,5 +30,7 @@ document.getElementById('qrForm').addEventListener('submit', function(event) {
     }else {
         alert('Please enter a valid link!');
     }
+
+    dataInput.value = '';
 
 });
