9e448f93bf3a61607cf176944d2d41071a53414c
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-11 15:42:00 +0300

    Update main.js

--- Small Projects/Background Remover/main.js
+++ Small Projects/Background Remover/main.js
@@ -2,13 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const removeButton = document.getElementById('remove-button');
     const outputImage = document.getElementById('output-image');
 
-    // Add an event listener to the Remove Background button
     removeButton.addEventListener('click', function() {
         console.log('Remove Background button clicked!');
         const formData = new FormData();
         formData.append('image', document.querySelector('input[type="file"]').files[0]);
 
-        // Send a POST request to the /process endpoint using Fetch API
         fetch('https://bg-remover-2-0.onrender.com/process', {
             method: 'POST',
             body: formData,
