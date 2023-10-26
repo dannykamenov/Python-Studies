c47a7b1872c6e2f904f7de05aec9ae1aa856a66e
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-26 22:21:27 +0300

    Update probability.py

--- probability.py
+++ probability.py
@@ -1,1 +1,17 @@
-#probability with python via Codecademy
+#probability with python via Codecademy!
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Let's say we have a normal six-sided die.
+# We can simulate the result of rolling the die with:
+# np.random.randint(1, 7)
+# The first argument is the lowest number possible to be rolled.
+# The second argument is the highest number possible to be rolled (plus one).
+# So in this case, the die can land on any number between 1 and 6 (inclusive).
+# We can use this function to simulate rolling a die multiple times.
+# For example, this code:
+# rolls = np.random.randint(1, 7, 100)
+# simulates rolling a six-sided die 100 times.
+# The result of each roll is stored in rolls.
+# We can use plt.hist to display the results of a simulation.
